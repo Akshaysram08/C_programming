977bf39f4c14eaf23cd3d73d57417050fb5e979a
Akshay Kumar MS <msakshay04@gmail.com>
2020-08-15 22:54:57 +0530

    Program to  take input from User

---
+++ Input.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a;
+    //to enter a number
+    printf("Enter a Number");
+    //to scan a number
+    scanf("%d",&a);
+    //to print the entered number
+    printf("The entered number is %d  :",a);
+    
+}

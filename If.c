8aa87967d2fa784369640d1a438c7bf4f617f13d
Akshay Kumar MS <msakshay04@gmail.com>
2020-08-16 21:39:29 +0530

    If loop-program to check whether a number is positive or negative

---
+++ If.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("enter a number");
+    scanf("%d",&num);
+    if (num>0)
+        {
+            printf("the number is positive");
+        }
+    else
+        {
+             printf("number is negative");
+        }
+}        
+  
